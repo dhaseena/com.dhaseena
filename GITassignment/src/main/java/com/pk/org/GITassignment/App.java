a54a1c4a7c25bfeb14bd2275ccc26da43f31a35c
Prokarma <Prokarma@Prokarma-PC>
2018-01-11 19:24:12 +0530

    Adding sample program

---
+++ GITassignment/src/main/java/com/pk/org/GITassignment/App.java
@@ -0,0 +1,13 @@
+package com.pk.org.GITassignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
